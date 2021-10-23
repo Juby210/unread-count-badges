31377e0dc831a4943fffcd042fe93d1b101105e2
Juby210 <31005896+Juby210@users.noreply.github.com>
2021-10-23 21:24:59 +0200

    rewrite the plugin

--- components/Settings.jsx
+++ components/Settings.jsx
@@ -1,13 +1,7 @@
 const { React } = require('powercord/webpack')
 const { SwitchItem } = require('powercord/components/settings')
 
-module.exports = class Settings extends React.Component {
-    render() {
-        return (<div>
-            <SwitchItem value={this.props.getSetting('ignoreMutedChannels')}
-            onChange={() => this.props.toggleSetting('ignoreMutedChannels')}>
-                Ignore muted channels
-            </SwitchItem>
-        </div>)
-    }
-}
+module.exports = ({ getSetting, toggleSetting }) => <SwitchItem
+    value={getSetting('ignoreMutedChannels')}
+    onChange={() => toggleSetting('ignoreMutedChannels')}
+>Ignore muted channels</SwitchItem>
--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 const { Plugin } = require('powercord/entities')
-const { getModule, FluxDispatcher, React } = require('powercord/webpack')
+const { getModule, React } = require('powercord/webpack')
 const { inject, uninject } = require('powercord/injector')
 
+const FluxUtils = getModule(['useStateFromStores'], false)
+
 const Settings = require('./components/Settings')
-const UpdateableBadge = require('./components/UpdateableBadge')
 
 module.exports = class UnreadCountBadges extends Plugin {
-    badges = []
-    lastGuildId = ""
-
     async startPlugin() {
         this.loadStylesheet('style.css')
         powercord.api.settings.registerSettings(this.entityID, {
@@ -17,36 +15,28 @@ module.exports = class UnreadCountBadges extends Plugin {
             render: Settings
         })
 
-        const { getUnreadCount } = await getModule(['getUnreadCount'])
-        const ChannelItem = await getModule(m => m.default && m.default.displayName == 'ChannelItem')
+        const { NumberBadge } = await getModule(['NumberBadge'])
+        const UnreadsStore = await getModule(['getUnreadCount'])
+        const UnreadBadge = ({ channelId }) => {
+            const unreadCount = FluxUtils.useStateFromStores([ UnreadsStore ], () => UnreadsStore.getUnreadCount(channelId))
+            if (!unreadCount) return null
+            return React.createElement(NumberBadge, { count: unreadCount, color: 'var(--background-accent)', className: 'ucbadge' })
+        }
+
+        const ChannelItem = await getModule(m => m.default && m.default.displayName === 'ChannelItem')
         inject('ucbadges', ChannelItem, 'default', args => {
             if (this.settings.get('ignoreMutedChannels') && args[0].muted) return args
 
-            const uc = getUnreadCount(args[0].channel.id)
-            if (uc && !args[0].children.find(c => c?.props?.className == 'ucbadge')) args[0].children.push(React.createElement(
-                UpdateableBadge, { className: 'ucbadge', channelId: args[0].channel.id, getUnreadCount, _this: this }
-            ))
+            if (!args[0].children.find(c => c?.type?.name === 'UnreadBadge'))
+                args[0].children.push(React.createElement(UnreadBadge, { channelId: args[0].channel.id }))
 
             return args
         }, true)
         ChannelItem.default.displayName = 'ChannelItem'
-
-        FluxDispatcher.subscribe('MESSAGE_CREATE', this.updateBadges = data => {
-            if (data.message.guild_id == this.lastGuildId)
-                this.badges.filter(b => b.props.channelId == data.message.channel_id).forEach(b => b.forceUpdate())
-        })
-        FluxDispatcher.subscribe('CHANNEL_SELECT', this.onSwitchChannel = data => {
-            if (this.lastGuildId != data.guildId) {
-                this.badges = []
-                this.lastGuildId = data.guildId
-            }
-        })
     }
 
     pluginWillUnload() {
         powercord.api.settings.unregisterSettings(this.entityID)
         uninject('ucbadges')
-        if (this.updateBadges) FluxDispatcher.unsubscribe('MESSAGE_CREATE', this.updateBadges)
-        if (this.onSwitchChannel) FluxDispatcher.unsubscribe('CHANNEL_SELECT', this.onSwitchChannel)
     }
 }
